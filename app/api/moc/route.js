59d175173f5532dac48b8107b846bb0fc3d94122
eastern-motors <easternmotorsin@gmail.com>
2024-12-26 20:03:39 +0530

    initial commit

---
+++ app/api/moc/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { Cell, base64ToBytes, bytesToBase64 } from "ton";
+
+export async function POST(req) {
+    try {
+        const { resultBoc } = await req.json();
+
+        const bocBytes = base64ToBytes(result.boc);
+
+        const cell = Cell.fromBoc(bocBytes)[0];
+
+        const bocHashBytes = cell.hash();
+
+        const hashBase64 = bytesToBase64(bocHashBytes);
+
+        let transaction_boc = resultBoc;
+        let transaction_boc_base64_hash = hashBase64;
+
+        return NextResponse.json({
+            transaction_boc,
+            transaction_boc_base64_hash,
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Failed to process request" });
+    }
+}
+
+export async function GET(req) {
+    return NextResponse.json({ status: "failed", message: "This endpoint is only for POST requests" });
+}
